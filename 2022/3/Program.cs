31f5f3648bbaf4b2afee4de9b5c2e938da2682d6
Chris Wood <16053164+ChrisWoody@users.noreply.github.com>
2022-12-21 08:09:59 +0800

    Completed 2022 day 3

--- 2022/3/Program.cs
+++ 2022/3/Program.cs
@@ -1,9 +1,33 @@
-﻿var input = File.ReadAllText("puzzle-input.txt");
+﻿var input = File.ReadAllLines("puzzle-input.txt");
 
-var partOne = 0;
+var partOnePriorities = input
+    .SelectMany(x => x[..(x.Length / 2)].Intersect(x.Substring(x.Length / 2, x.Length / 2)).Distinct())
+    .Select(GetPriority)
+    .ToArray();
+
+var partOne = partOnePriorities.Sum();
 
 Console.WriteLine($"Part 1: {partOne}");
 
-var partTwo = 0;
+var partTwoPriorities = input
+    .Chunk(3)
+    .Select(x => x.First().Intersect(x.Skip(1).First()).Intersect(x.Skip(2).First())
+        .Distinct()
+        .GroupBy(e => e)
+        .OrderByDescending(e => e.Count()).First().Key)
+    .Select(GetPriority)
+    .ToArray();
+
+var partTwo = partTwoPriorities.Sum();
 
 Console.WriteLine($"Part 2: {partTwo}");
+
+static int GetPriority(char c)
+{
+    if (char.IsLower(c))
+    {
+        return c - 'a' + 1;
+    }
+
+    return c - 'A' + 27;
+}
