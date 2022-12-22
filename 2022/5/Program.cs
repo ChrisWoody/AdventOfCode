dc5d730fdd3a6587a7be1ec999cc040958a119b5
Chris Wood <16053164+ChrisWoody@users.noreply.github.com>
2022-12-22 15:15:37 +0800

    Add 2022 day 5

---
+++ 2022/5/Program.cs
@@ -0,0 +1,46 @@
+﻿var input = File.ReadAllLines("puzzle-input.txt");
+
+var stacks = Enumerable.Range(0, 9)
+    .Select(y => new Stack<char>(Enumerable.Range(0, 8)
+        .Select(x => input[x][y * 4 + 1]).Where(x => x != ' ').Reverse())).ToArray();
+
+foreach (var line in input.Skip(10))
+{
+    var offset = line.Length == 19 ? 1 : 0;
+
+    var count = int.Parse(line.Substring(5, 1 + offset).Trim());
+    var oldStack = int.Parse(line[12 + offset].ToString()) - 1;
+    var newStack = int.Parse(line[17 + offset].ToString()) - 1;
+
+    for (var i = 0; i < count; i++)
+        stacks[newStack].Push(stacks[oldStack].Pop());
+}
+
+var partOne = new string(stacks.Select(x => x.Peek()).ToArray());
+
+Console.WriteLine($"Part 1: {partOne}");
+
+stacks = Enumerable.Range(0, 9)
+    .Select(y => new Stack<char>(Enumerable.Range(0, 8)
+        .Select(x => input[x][y * 4 + 1]).Where(x => x != ' ').Reverse())).ToArray();
+
+
+foreach (var line in input.Skip(10))
+{
+    var offset = line.Length == 19 ? 1 : 0;
+
+    var count = int.Parse(line.Substring(5, 1 + offset).Trim());
+    var oldStack = int.Parse(line[12 + offset].ToString()) - 1;
+    var newStack = int.Parse(line[17 + offset].ToString()) - 1;
+
+    var craneStack = new Stack<char>();
+    for (var i = 0; i < count; i++)
+        craneStack.Push(stacks[oldStack].Pop());
+
+    for (var i = 0; i < count; i++)
+        stacks[newStack].Push(craneStack.Pop());
+}
+
+var partTwo = new string(stacks.Select(x => x.Peek()).ToArray());
+
+Console.WriteLine($"Part 2: {partTwo}");
